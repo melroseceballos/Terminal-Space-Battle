b8820d2d22ef84f0637338e3d01a50c77ef14182
Melrose Ceballos <melroseceballos@Melroses-MBP.lan>
2023-02-13 22:49:52 -0800

    Creation of player

--- app.js
+++ app.js
@@ -1,140 +1,24 @@
-class Aliens {
-    constructor (hull,firepower,accuracy){
-        this.hull = Math.floor(Math.random() * 4 + 3);
-        this.firepower = Math.floor(Math.random () * 2 + 3 );
-        this.accuracy = (Math.random()* .2 + .6);
-        attack (damage);{
-            if (Math.random() < alien[0].accuracy) {
-                console.log('You have been hit!');
-                damage.hull = damage.hull - this.firepower
-        }else if (Math.random() > alien[0].accuracy) {
-            console.log('Missed!');
-    }
-        }
-        }
-    }
-    //Alien1
-    class AlienOne {
-        constructor (hull,firepower,accuracy){
-            this.hull = Math.floor(Math.random() * 4 + 3);
-            this.firepower = Math.floor(Math.random () * 2 + 3 );
-            this.accuracy = (Math.random()* .2 + .6);
-            attack (damage);{
-                if (Math.random() < alien[0].accuracy) {
-                    console.log('You have been hit!');
-                    damage.hull = damage.hull - this.firepower
-            }else if (Math.random() > alien[0].accuracy) {
-                console.log('Missed!');
+// Require prompt sync
+ const prompt = require ('prompt-sync') (); 
+
+// Create Parent class for Ship 
+class Ship { 
+    constructor (name,hull,firepower,accuracy){ 
+        this.name = name 
+        this.hull = hull 
+        this.firepower = firepower 
+        this.accuracy = accuracy 
+    } 
+} 
+// Creating USS Schwarz
+const player = new Ship ("USS Shwarz", 20, 5, .7)
+
+// setting values for Alien Ship
+class AlienShip extends Ship {
+     constuctor(hull,firepower,accuracy){ 
+        this.hull = Math.floor(Math.random () * 4 + 3) 
+        this.firepower = Math.floor(Math.random () * 3 + 2)
+         this.accuracy = Math.floor(Math.random () * .6 + .2);
+         } 
         }
         
-            }
-            }
-        }
-        //Alien-2
-        class AlienTwo {
-            constructor (hull,firepower,accuracy){
-                this.hull = Math.floor(Math.random() * 4 + 3);
-                this.firepower = Math.floor(Math.random () * 2 + 3 );
-                this.accuracy = (Math.random()* .2 + .6);
-                attack (damage);{
-                    if (Math.random() < alien[0].accuracy) {
-                        console.log('You have been hit!');
-                        damage.hull = damage.hull - this.firepower
-                }else if (Math.random() > alien[0].accuracy) {
-                    console.log('Missed!');
-            }
-            
-                }
-                }
-            }
-            //Alien-3
-            class AlienThree {
-                constructor (hull,firepower,accuracy){
-                    this.hull = Math.floor(Math.random() * 4 + 3);
-                    this.firepower = Math.floor(Math.random () * 2 + 3 );
-                    this.accuracy = (Math.random()* .2 + .6);
-                    attack (damage);{
-                        if (Math.random() < alien[0].accuracy) {
-                            console.log('You have been hit!');
-                            damage.hull = damage.hull - this.firepower
-                    }else if (Math.random() > alien[0].accuracy) {
-                        console.log('Missed!');
-                }
-                
-                    }
-                    }
-                }
-                //Alien-4
-                class AlienFour {
-                    constructor (hull,firepower,accuracy){
-                        this.hull = Math.floor(Math.random() * 4 + 3);
-                        this.firepower = Math.floor(Math.random () * 2 + 3 );
-                        this.accuracy = (Math.random()* .2 + .6);
-                        attack (damage);{
-                            if (Math.random() < alien[0].accuracy) {
-                                console.log('You have been hit!');
-                                damage.hull = damage.hull - this.firepower
-                        }else if (Math.random() > alien[0].accuracy) {
-                            console.log('Missed!');
-                    }
-                    
-                        }
-                        }
-                    }
-                    //Alien-5
-                    class AlienFive {
-                        constructor (hull,firepower,accuracy){
-                            this.hull = Math.floor(Math.random() * 4 + 3);
-                            this.firepower = Math.floor(Math.random () * 2 + 3 );
-                            this.accuracy = (Math.random()* .2 + .6);
-                            attack (damage);{
-                                if (Math.random() < alien[0].accuracy) {
-                                    console.log('You have been hit!');
-                            }else if (Math.random() > alien[0].accuracy) {
-                                console.log('Missed!');
-                        }
-                        
-                            }
-                            }
-                        }
-                        //Alien-6
-                        class AlienSix {
-                            constructor (hull,firepower,accuracy){
-                                this.hull = Math.floor(Math.random() * 4 + 3);
-                                this.firepower = Math.floor(Math.random () * 2 + 3 );
-                                this.accuracy = (Math.random()* .2 + .6);
-                                attack (damage);{
-                                    if (Math.random() < alien[0].accuracy) {
-                                        console.log('You have been hit!');
-                                        damage.hull = damage.hull - this.firepower
-                                }else if (Math.random() > alien[0].accuracy) {
-                                    console.log('Missed!');
-                            }
-                            
-                                }
-                                }
-                            }
-/** ***************************************************************************************************** */
-    class USSSchwarz extends Alien {
-        constructor (hull,firepower,accuracy, life);
-            super(hull, firepower, accuracy,life)
-            this.hull = 20;
-            this.firepower = 5;
-            this.accuracy = .7;
-        }   life (meter){
-            if (hull <= 0){
-                console.log ("Your ship's Kablooey")
-            } else {
-                console.log ("You're still alive! " + this.hull)
-            }
-        }
-/********************************************************************************************************* */     
-   class Game (Alien, AlienOne, AlienTwo, AlienThree, AlienFour, AlienFive, AlienSix, USSSchwarz) {
-while(USSSchwarz.hull > 0){
-    USSSchwarz.attack(Alien, AlienOne,AlienTwo, AlienThree, AlienFour, AlienFive, AlienSix);
-}
-    while (Alien.hull, AlienOne.hull,AlienTwo.hull, AlienThree.hull, AlienFour.hull, AlienFive.hull, AlienSix.hull > 0){
-        Alien.attack, AlienOne.attack, AlienTwo.attack, AlienThree.attack, AlienFour.attack, AlienFive.attack, AlienSix.attack(USSSchwarz)
-    }
-}
-   
